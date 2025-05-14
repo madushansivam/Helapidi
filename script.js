7a03dddcaa4adb06470ddc0a8de10ff90532139f
Subashini0118 <subashinisuba20020118@gmail.com>
2025-05-14 11:13:06 +0530

    Create script.js
    class animation
    update

---
+++ script.js
@@ -0,0 +1,43 @@
+//BAD/IT/2324/F/076(starting)
+//animation class
+class animation{
+  constructor(spriteKey='placeholder',startIndex=0,numIndicates=1,framesPerIndex=5,loop=true{
+    this.spriteKey=spriteKey;
+    this.startIndex=startIndex;
+    this.numIndicates=numIndicates;
+    this.framesPerIndex=framesPerIndex;
+    this.loop=loop;
+    
+    this.isDone=false;
+    this.frame=0;
+    this.index=0;
+    this.pauseFrames=0;
+  }
+
+update(){
+  if(this.isDone){
+    return;
+  }
+
+  if(this.pauseFrames>0){
+    this.pauseFrames--;
+    return;
+  }
+
+  this.frame++;
+  if(this.frame===this.framesPerIndex){
+    this.frame=0;
+    this.index++;
+
+  if(this.index===this.numIndices){
+    if(this.loop){
+      this.frame=0;
+      this.index=0;
+    }else{
+      this.index=this.numIndices-1;
+      this.isDone=true;
+    }
+  }
+  }
+}
+  //BAD/IT/2324/F/076(FINISHED)
